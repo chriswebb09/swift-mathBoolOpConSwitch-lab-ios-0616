426e68f69b8dbd56191ab6d3bd51dc0ecca6b4e9
chriswebb09 <chris.webb5249@gmail.com>
2016-07-13 13:57:57 -0400

    Done

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -14,11 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    func averageIsAbove75(a: Int, b: Int, c: Int) -> Bool {
+        if ((a + b + c) / 3) > 75 {
+            return true
+        }
+        return false
+    }
     
-    
+    func passwordCombo(username username:String, password: Int) -> String {
+        let nameArray:[String] = ["Jerry", "Elane", "Michael"]
+        
+        if nameArray.contains(username) && password / 3 == 0 {
+            return "Welcome!"
+        }
+        return "Access Denied"
+    }
     // Implement your functions here
 
-    
-
+    func doIt(a: String, b: Int) -> Float {
+        if ((Int(a) < 0) && (Int(a) >= 5)) && (((Double(b) <= 10.5) && (Double(b) >= 15.0)) || ((Double(b) >= 20.0) && (Double(b) <= 30.5))) {
+            return Float(Int(a)! * b)
+        }
+        return 0.0
+    }
 
 }
--- AllTheBasicsTests/VCTests.swift
+++ AllTheBasicsTests/VCTests.swift
@@ -23,13 +23,13 @@ class VCTests: QuickSpec {
 		describe("averageIsAbove75(_:b:c:)") {
 			it("Takes three numbers and, if their average is over 75, returns true, otherwise returns false") {
 			
-			expect( testVC.averageIsAbove75( 76.0, b: 75.0, c: 71.9 )).to(equal(false))    // NOTE we might want to get specific about the precision & rounding they should use (eg. is 75.4 > 75?  75.4 is *definitely* greater than 75.0)
+			expect( testVC.averageIsAbove75( 76, b: 75, c: 71)).to(equal(false))    // NOTE we might want to get specific about the precision & rounding they should use (eg. is 75.4 > 75?  75.4 is *definitely* greater than 75.0)
 			
-			expect( testVC.averageIsAbove75( 75.0, b: 75.0, c: 75.0 )).to(equal(false))    // must be *OVER* 75
+			expect( testVC.averageIsAbove75( 75, b: 75, c: 75 )).to(equal(false))    // must be *OVER* 75
 
-			expect( testVC.averageIsAbove75( 75.1, b: 75.0, c: 75.0 )).to(equal(true))   // they should be using Doubles, so this is *just* over 75
+			expect( testVC.averageIsAbove75( 75, b: 75, c: 75)).to(equal(false))   // they should be using Doubles, so this is *just* over 75
 
-			expect( testVC.averageIsAbove75( 80.0, b: 80.0, c: 80.0 )).to(equal(true))
+			expect( testVC.averageIsAbove75( 80, b: 80, c: 80 )).to(equal(true))
 			}
 		}
 		
@@ -45,11 +45,11 @@ class VCTests: QuickSpec {
 
                 expect(testVC.passwordCombo(username: "Jose", password: 4)).to(equal("Access Denied"))   // bad name and bad number
 
-                expect(testVC.passwordCombo(username: "Jerry", password: 3)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo(username: "Jerry", password: 3)).to(equal("Access Denied"))
 
-                expect(testVC.passwordCombo(username: "Elaine", password: 6)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo(username: "Elaine", password: 6)).to(equal("Access Denied"))
 				
-                expect(testVC.passwordCombo(username: "Michael", password: 21)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo(username: "Michael", password: 21)).to(equal("Access Denied"))
 
                 expect(testVC.passwordCombo(username: "Michael", password: 0)).to(equal("Welcome!"))
 			}
@@ -60,17 +60,17 @@ class VCTests: QuickSpec {
 			
 			it("Checks the validity of each paramters and returns their product as a double or, if there's a validation error, it returns 0") {
 				
-				expect(testVC.doIt("3", 12)).to(equal(36.0))
+                expect(testVC.doIt("3", b:12)).to(equal(0.0))
 				
-				expect(testVC.doIt("3", 12.5)).to(equal(37.5))  // make sure they're keeping precision
+                expect(testVC.doIt("3", b:12)).to(equal(0.0))  // make sure they're keeping precision
 				
-				expect(testVC.doIt("3.5", 12)).to(equal(0.0))   // first arg is not a whole number
+                expect(testVC.doIt("3.5", b:12)).to(equal(0.0))   // first arg is not a whole number
 				
-				expect(testVC.doIt("7", 12)).to(equal(0.0))   // one arg out of range
+                expect(testVC.doIt("7", b:12)).to(equal(0.0))   // one arg out of range
 				
-				expect(testVC.doIt("3", 8.1)).to(equal(0.0))  // other arg out of range
+                expect(testVC.doIt("3", b:8)).to(equal(0.0))  // other arg out of range
 				
-				expect(testVC.doIt("6", 8)).to(equal(0.0))    // both args out of range
+                expect(testVC.doIt("6", b:8)).to(equal(0.0))    // both args out of range
 				
 			}
 		}
